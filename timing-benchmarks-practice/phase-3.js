36791751ce39c4c539882edad8cb99bc38aa23cb
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-22 16:48:10 +0530

    😎 Phase 3 Solved

--- timing-benchmarks-practice/phase-3.js
+++ timing-benchmarks-practice/phase-3.js
@@ -1,29 +1,33 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
-
-  // Your code here
-
+function addNums10Timing(increament) {
+  let Arr = [];
+  console.time("addNums10");
+  for (let i = 1; i <= 10; i++) {
+    Arr.push(addNums(increament * i));
+    console.timeLog("addNums10");
+    console.log(Date.now());
+  }
+  console.timeEnd("addNums10");
 }
 
-
-function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
-
-  // Your code here
-
+function addManyNums10Timing(increament) {
+  let Arr = [];
+  console.time("addManyNums10");
+  for (let i = 1; i <= 10; i++) {
+    Arr.push(addManyNums(increament * i));
+    console.timeLog("addManyNums10");
+    console.log(Date.now());
+  }
+  console.timeEnd("addManyNums10");
 }
 
-
-n = 1000000
+n = 1000000;
 console.log(`addNums(${n}): `);
 addNums10Timing(1000000);
 
 console.log("\n***********\n");
 
-n = 1000
+n = 1000;
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
