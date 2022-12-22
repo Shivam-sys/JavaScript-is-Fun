d7771c5689190904c32222abfffc2e049707b549
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-22 16:29:22 +0530

    😎 Phase 2 Solved

--- timing-benchmarks-practice/phase-2.js
+++ timing-benchmarks-practice/phase-2.js
@@ -1,15 +1,33 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-// Runs `addNums` in 10 increasing increments
-function addNums10(increment) {
-  // Fill this in
+// !Try using closure - going infinite loop
+// function addNumTimes(times) {
+//   return function addNums(increament) {
+//     let Arr = [];
+//     for (let i = 1; i <= times; i++) {
+//       Arr.push(addNums(increament * i));
+//     }
+//     return Arr;
+//   };
+// }
+// const addNums10 = addNumTimes(10);
 
+// Runs `addNums` in 10 increasing increments
+function addNums10(increament) {
+  let Arr = [];
+  for (let i = 1; i <= 10; i++) {
+    Arr.push(addNums(increament * i));
+  }
+  return Arr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
-function addManyNums10(increment) {
-  // Fill this in
-
+function addManyNums10(increament) {
+  let Arr = [];
+  for (let i = 1; i <= 10; i++) {
+    Arr.push(addManyNums(increament * i));
+  }
+  return Arr;
 }
 
 module.exports = [addNums10, addManyNums10];
