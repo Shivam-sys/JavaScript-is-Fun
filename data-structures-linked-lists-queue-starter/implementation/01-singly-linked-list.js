e43cf3db9be12f0432c61f2275a240027edf808d
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-25 22:03:29 +0530

    😎 Phase 1 Solved

--- data-structures-linked-lists-queue-starter/implementation/01-singly-linked-list.js
+++ data-structures-linked-lists-queue-starter/implementation/01-singly-linked-list.js
@@ -1,70 +1,111 @@
 // Node class is implemented for you, no need to look for bugs here!
 class SinglyLinkedNode {
-    constructor(val) {
-        this.value = val;
-        this.next = null;
-    }
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // Add node of val to head of linked list
+    let node = new SinglyLinkedNode(val);
+    if (this.head) {
+      node.next = this.head;
+    }
+    this.head = node;
+    this.length++;
+    return this;
+    // Write your hypothesis on the time complexity of this method here
+    // Time complexity should be O(1) here.
+  }
+
+  addToTail(val) {
+    // There are bugs in this method! Fix them!!!
+    // Write your hypothesis on the time complexity of this method here
+
+    // Add node of val to tail of linked list
+    let newNode = new SinglyLinkedNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.length++;
+      return this;
     }
 
-    addToHead(val) { 
-        // Add node of val to head of linked list
-
-        // Write your hypothesis on the time complexity of this method here
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
     }
+    curr.next = newNode;
+    this.length++;
 
-    addToTail(val) {
-        // There are bugs in this method! Fix them!!!
-        // Write your hypothesis on the time complexity of this method here
+    return this;
+  }
 
-        // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
-
-        if (!head) {
-            head = newNode;
-            return head;
-        }
-
-        let curr = head;
-        while (curr) {
-            curr = current.next;
-        }
-        curr.next = newNode;
-
-        return head;
+  removeFromHead() {
+    if (!this.head) {
+      return;
     }
+    let ret = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return ret;
+  }
 
-    removeFromHead() {
-        // Remove node at head
-
-        // Write your hypothesis on the time complexity of this method here
+  removeFromTail() {
+    if (!this.head) {
+      return;
     }
-
-    removeFromTail() {
-        // Remove node at tail
-
-        // Write your hypothesis on the time complexity of this method here
+    if (!this.head.next) {
+      let ret = this.head;
+      this.head = null;
+      this.length--;
+      return ret;
     }
-
-    peekAtHead() {
-        // Return value of head node
-
-        // Write your hypothesis on the time complexity of this method here
+    let curr = this.head;
+    while (curr.next.next) {
+      curr = curr.next;
     }
+    let ret = curr.next;
+    curr.next = null;
+    this.length--;
+    return ret;
+  }
 
-    print() {
-        // Print out the linked list
-        
-        // Write your hypothesis on the time complexity of this method here
+  peekAtHead() {
+    // Return value of head node
+    // Write your hypothesis on the time complexity of this method here
+    if (!this.head) {
+      return;
     }
+    return this.head.value;
+    // Time complexity seems O(1).
+  }
+
+  print() {
+    // Print out the linked list
+    // Write your hypothesis on the time complexity of this method here
+    if (!this.head) {
+      return;
+    }
+    let curr = this.head;
+    while (curr) {
+      // process.stdout.write(`${curr.value} -> `);
+      console.log(curr.value);
+      curr = curr.next;
+    }
+    // process.stdout.write("NULL");
+    // Time complexity seems O(n), each item is iterated once.
+  }
 }
 
 module.exports = {
-    SinglyLinkedList,
-    SinglyLinkedNode
-}
+  SinglyLinkedList,
+  SinglyLinkedNode,
+};
