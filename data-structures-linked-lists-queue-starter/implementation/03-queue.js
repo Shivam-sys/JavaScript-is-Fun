d2cc7ecc7cf58615c1c580a5b0bbe1fd9c60f350
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-25 23:24:54 +0530

    😎 Phase 3 Solved- basic of LL, DLL, queue

--- data-structures-linked-lists-queue-starter/implementation/03-queue.js
+++ data-structures-linked-lists-queue-starter/implementation/03-queue.js
@@ -1,27 +1,42 @@
 const { SinglyLinkedNode } = require("./01-singly-linked-list");
 
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    constructor() {
-        this.head = null;
-        this.tail = null;
+  enqueue(val) {
+    // Add node to end of queue (linked list)
+
+    // Write your hypothesis on the time complexity of this method here - O(1)
+    let node = new SinglyLinkedNode(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
     }
+    return ++this.length;
+  }
 
-    enqueue(val) {
-        // Add node to end of queue (linked list)
-
-        // Write your hypothesis on the time complexity of this method here
+  dequeue() {
+    // Remove node from front of queue (linked list)
+    // Write your hypothesis on the time complexity of this method here - O(1)
+    if (!this.head) return null;
+    let ret = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
     }
-
-    dequeue() {
-        // Remove node from front of queue (linked list)
-        
-        // Write your hypothesis on the time complexity of this method here
-    }
-
+    this.length--;
+    return ret;
+  }
 }
 
 module.exports = {
-    Queue,
-    SinglyLinkedNode
-}
+  Queue,
+  SinglyLinkedNode,
+};
