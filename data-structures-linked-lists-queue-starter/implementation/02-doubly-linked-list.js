0c35b9ee5da5b26bd0525a7124b238af78b724c0
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-25 22:54:39 +0530

    😎 Phase 2 Solved

--- data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
+++ data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
@@ -1,70 +1,109 @@
 // Node class is implemented for you, no need to look for bugs here!
 class DoublyLinkedNode {
-    constructor(val) {
-        this.value = val;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // There are bugs in this method! Fix them!!!
+    // Write your hypothesis on the time complexity of this method here
+
+    // Add node of val to head of linked list
+    let newNode = new DoublyLinkedNode(val);
+
+    if (this.length > 0) {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    } else {
+      this.head = this.tail = newNode;
     }
 
-    addToHead(val) {
-        // There are bugs in this method! Fix them!!!
-        // Write your hypothesis on the time complexity of this method here
-
-        // Add node of val to head of linked list
-        let newNode = new DoublyLinkedNode(val);
-
-        if (this.length >= 0) {
-            this.head.previous = newNode;
-            newNode.next = this.head;
-            this.head = newNode;
-        } else {
-            this.head = newNode;
-            this.tail = newNode;
-        }
-
-        this.length++;
+    this.length++;
+    // Time complexity - O(1)
+  }
+  print() {
+    if (!this.head) {
+      return;
     }
-
-    addToTail(val) {
-        // Add node of val to tail of linked list
-
-        // Write your hypothesis on the time complexity of this method here
+    let curr = this.head;
+    while (curr) {
+      process.stdout.write(`${curr.value} -> `);
+      console.log(curr.value);
+      curr = curr.next;
     }
+    process.stdout.write("NULL");
+  }
 
-    removeFromHead() {
-        // Remove node at head
-
-        // Write your hypothesis on the time complexity of this method here
+  addToTail(val) {
+    // Add node of val to tail of linked list
+    // Write your hypothesis on the time complexity of this method here
+    const newNode = new DoublyLinkedNode(val);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      this.length++;
+      return;
     }
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    //Time Complexity - O(1)
+  }
 
-    removeFromTail() {
-        // Remove node at tail
-
-        // Write your hypothesis on the time complexity of this method here
+  removeFromHead() {
+    // Remove node at head
+    // Write your hypothesis on the time complexity of this method here
+    if (!this.head) {
+      return;
     }
+    let ret = this.head.value;
+    this.head = this.head.next;
+    if (this.head) this.head.prev = null;
+    this.length--;
+    return ret;
+    // TC seems O(1)
+  }
 
-    peekAtHead() {
-        // Return value of head node
-
-        // Write your hypothesis on the time complexity of this method here
+  removeFromTail() {
+    // Remove node at tail
+    // Write your hypothesis on the time complexity of this method here - O(1)
+    if (!this.head) {
+      return;
     }
+    let ret = this.tail.value;
+    if (this.tail.prev) this.tail.prev.next = null;
+    this.tail = this.tail.prev;
+    this.length--;
+    return ret;
+  }
 
-    peekAtTail() {
-        // Return value of tail node
-        
-        // Write your hypothesis on the time complexity of this method here
-    }
+  peekAtHead() {
+    // Return value of head node
+    // Write your hypothesis on the time complexity of this method here
+    if (!this.head) return;
+    return this.head.value;
+  }
+
+  peekAtTail() {
+    // Return value of tail node
+    // Write your hypothesis on the time complexity of this method here
+    if (!this.tail) return;
+    return this.tail.value;
+  }
 }
 
 module.exports = {
-    DoublyLinkedList,
-    DoublyLinkedNode
-}
+  DoublyLinkedList,
+  DoublyLinkedNode,
+};
