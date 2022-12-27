29af1e7c388a0cb589bcf8ee673a6c052abe898c
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-28 02:07:53 +0530

    PENDING: to implement resize hashtable

--- hash-tables-long-practice/phases/01-implementation.js
+++ hash-tables-long-practice/phases/01-implementation.js
@@ -6,11 +6,14 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
     // Initialize your buckets here
-    // Your code here
+    this.data = new Array(numBuckets).fill(null);
+    this.capacity = numBuckets;
+    this.count = 0;
   }
 
   hash(key) {
@@ -28,26 +31,55 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
+  _getNodeWithMatchingKey(head, key) {
+    let current = head;
+    while (current) {
+      if (current.key === key) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
+  }
 
   insert(key, value) {
-    // Your code here
+    const kvpair = new KeyValuePair(key, value);
+    const index = this.hashMod(key);
+    let node = this._getNodeWithMatchingKey(this.data[index], kvpair.key);
+    if (this.data[index]) {
+      if (node) {
+        node.value = kvpair.value;
+      } else {
+        kvpair.next = this.data[index];
+        this.data[index] = kvpair;
+        this.count++;
+      }
+    } else {
+      this.data[index] = kvpair;
+      this.count++;
+    }
+    if (this.count >= this.capacity) this.resize();
   }
-
 
   read(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let node = this._getNodeWithMatchingKey(this.data[index], key);
+    if (node) return node.value;
+    return;
   }
-
 
   resize() {
-    // Your code here
+    // TODO: Implement this.
+    let newHashTable = new HashTable(this.capacity * 2);
+    for (let i = 0; i < this.count; i++) {
+      newHashTable.data[i] = this.data[i];
+    }
+    newHashTable.count = this.count;
   }
-
 
   delete(key) {
     // Your code here
   }
 }
-
 
 module.exports = HashTable;
