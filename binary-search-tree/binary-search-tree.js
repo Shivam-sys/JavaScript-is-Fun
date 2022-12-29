0d9a75d282b4663825641a710526fc7950a5d19d
Shivam <tinkal.kumar.3766@gmail.com>
2022-12-29 22:55:45 +0530

    Climbed Binary Search Tree 🐒

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -8,43 +8,103 @@ class TreeNode {
 }
 
 class BinarySearchTree {
-
   constructor() {
-    // Your code here
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
-    // Your code here
+  _insertNode(newNode, currentNode) {
+    if (newNode.val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+      } else this._insertNode(newNode, currentNode.left);
+    } else {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+      } else this._insertNode(newNode, currentNode.right);
+    }
   }
 
-  search(val) {
-    // Your code here
+  insert(val, currentNode = this.root) {
+    let newNode = new TreeNode(val);
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this._insertNode(newNode, currentNode);
+    }
   }
 
+  //Comment the line below incase of Iterative.
+  search(val, currentNode = this.root) {
+    //Uncomment the line below incase of Iterative.
+    // search(val) {
+    // * Recursive Approach to find an element in BST
+    if (!currentNode) return false;
+    else if (val < currentNode.val) return this.search(val, currentNode.left);
+    else if (val > currentNode.val) return this.search(val, currentNode.right);
+    else return true;
+    // // *Iterative Approach to find an element in BST
+    // let currentNode = this.root;
+    // while (currentNode) {
+    //   if (val < currentNode.val) currentNode = currentNode.left;
+    //   else if (val > currentNode.val) currentNode = currentNode.right;
+    //   else return true;
+    // }
+    // return false;
+  }
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (!currentNode) {
+      return;
+    }
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
   }
-
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (!currentNode) {
+      return;
+    }
+    this.inOrderTraversal(currentNode.left);
+    console.log(currentNode.val);
+    this.inOrderTraversal(currentNode.right);
   }
-
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (!currentNode) {
+      return;
+    }
+    this.postOrderTraversal(currentNode?.left);
+    this.postOrderTraversal(currentNode?.right);
+    console.log(currentNode?.val);
   }
 
-    // Breadth First Traversal - Iterative
+  //* Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // your code here
+    // * Using Queue
+    let currentNode = this.root;
+    let queue = [];
+    queue.push(currentNode);
+    while (queue.length > 0) {
+      let node = queue.shift();
+      console.table(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
   }
 
-  // Depth First Traversal - Iterative
+  //* Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // your code here
-}
+    let currentNode = this.root;
+    let stack = [];
+    stack.push(currentNode);
+    while (stack.length > 0) {
+      let node = stack.pop();
+      console.log(node.val);
+      if (node.left) stack.push(node.left);
+      if (node.right) stack.push(node.right);
+    }
+  }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
