fc1b334841fe863f03c5e5071d04598cf82dae23
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-04 14:07:00 +0530

    parsed form requested data

--- http-basics-practice/parse-request-body/parse-body.js
+++ http-basics-practice/parse-request-body/parse-body.js
@@ -1,25 +1,33 @@
 function firstStep(input) {
-  // Your code here
+  return input.split("&");
 }
 
 function secondStep(input) {
-  // Your code here
+  return input.map((keyvalPair) => keyvalPair.split("="));
 }
 
 function thirdStep(input) {
-  // Your code here
+  return input.map(([key, value]) => [key, value.replace(/\+/g, " ")]);
 }
 
 function fourthStep(input) {
-  // Your code here
+  return input.map(([key, value]) => [key, decodeURIComponent(value)]);
 }
 
 function fifthStep(input) {
-  // Your code here
+  return input.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
 }
 
 function parseBody(str) {
-  // Your code here
+  str = firstStep(str);
+  str = secondStep(str);
+  str = thirdStep(str);
+  str = fourthStep(str);
+  str = fifthStep(str);
+  return str;
 }
 
 /******************************************************************************/
@@ -31,5 +39,5 @@ module.exports = {
   thirdStep,
   fourthStep,
   fifthStep,
-  parseBody
+  parseBody,
 };
--- http-basics-practice/parse-request-body/server.js
+++ http-basics-practice/parse-request-body/server.js
@@ -5,7 +5,24 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here
+const http = require("http");
+const PORT = 5000;
+
+server = http.createServer((req, res) => {
+  let reqBody = "";
+  req.on("data", (data) => {
+    reqBody += data;
+  });
+
+  req.on("end", () => {
+    if (reqBody) req.body = parseBody(reqBody);
+    sendFormPage(req, res);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Successfully started the server on port ${PORT}`);
+});
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
