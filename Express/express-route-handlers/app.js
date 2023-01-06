1a4b1a12ef4b32a55ad337dbbba56654b1a4d9de
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-06 14:46:20 +0530

    ✅ Express route handled

--- Express/express-route-handlers/app.js
+++ Express/express-route-handlers/app.js
@@ -18,13 +18,52 @@ const {
   getSongBySongId,
   addSongByAlbumId,
   editSongBySongId,
-  deleteSongBySongId
-} = require('./data');
+  deleteSongBySongId,
+} = require("./data");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// Your code here
+// any body in a request that is made with a 'Content-Type: of application/json' header,
+// will be deserialized and saved to the body property on the request object, req.body.
+app.use(express.json());
 
+/* Test the above comment using this code.*/
+/* app.use((req, res, next) => {
+  console.log("Body:", req.body);
+  next();
+}); */
+
+app.get("/", (req, res) => {
+  res.send(req.body);
+});
+
+app.get("/artists", (req, res) => {
+  const allArtists = getAllArtists();
+  res.status(200).header("Content-type: application/json").json(allArtists);
+});
+
+app.post("/artists", (req, res) => {
+  const newArtist = addArtist(req.body);
+  res.status(201).header("Content-type: application/json").json(newArtist);
+});
+
+app.get("/artists/latest", (req, res) => {
+  const latestArtist = getLatestArtist();
+  res.status(200).header("Content-type: application/json").json(latestArtist);
+});
+
+app.get("/artists/latest/albums", (req, res) => {
+  const latestArtistAlbums = getAlbumsForLatestArtist();
+  res
+    .status(200)
+    .header("Content-type: application/json")
+    .json(latestArtistAlbums);
+});
+
+app.post("/albums", (req, res) => {
+  const newAlbum = addAlbumByArtistId(req.body);
+  res.status(201).header("Content-type: application/json").json(newAlbum);
+});
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
