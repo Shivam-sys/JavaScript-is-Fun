eb1ba86f4871cc3d205fe33774ee38074974b048
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-07 09:07:43 +0530

    Explored Middleware

--- Express/app.js
+++ Express/app.js
@@ -9,12 +9,31 @@ const data = {
   users: [],
 };
 
+// Middleware functions
+const logTime = (req, res, next) => {
+  console.log("Current time: ", new Date().toISOString());
+  next();
+};
+
+// `app.use(logTime)` an application-level middleware- invoked everytime the app recieves a request.
+app.use(logTime);
+
+const passOnMessage = (req, res, next) => {
+  console.log("Passing on a message!");
+  req.passedMessage = "Hello from passOnMessage!";
+  next();
+};
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.get("/status", (req, res) => {
-  res.send("The server is alive!");
+// bind the app object with the `get` method with the arguments "/status" route,
+// `logTime` function, and `req`, `res`.
+const middlewares = [logTime, passOnMessage];
+app.get("/status", middlewares, (req, res) => {
+  // app.get("/status", logTime, passOnMessage, (req, res) => {
+  res.send("The server is alive!" + req.passedMessage);
 });
 
 app.get("/users", (req, res) => {
