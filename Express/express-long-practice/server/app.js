71bbdaebb8ccb078ade526de72f87fdcc6565b23
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-09 23:18:15 +0530

    handle errrrs again📝

--- Express/express-long-practice/server/app.js
+++ Express/express-long-practice/server/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
+require("dotenv").config();
+
 const dogs = require("./routes/dogs");
+
 // Use set NODE_ENV=production , to hide error log from non admins.
 // NOTE: express-async-errors prevents server to crash incase of errors, so must use.
 require("express-async-errors");
+
 const app = express();
-
 app.use(express.json());
-
 app.use("/dogs", dogs);
+app.use("/static", express.static("assets"));
 
+// Middleware to log request method and path, response's status.
 const logger = (req, res, next) => {
   console.log(req.method, req.url);
   // use this listener to handle res object once res is done manipulated.
@@ -19,7 +23,6 @@ const logger = (req, res, next) => {
   next();
 };
 
-app.use("/static", logger, express.static("assets"));
 // For testing purposes, GET /
 app.get("/", logger, (req, res) => {
   res.json(
@@ -40,10 +43,18 @@ app.get("/test-error", logger, async (req, res) => {
   throw new Error("Hello World!");
 });
 
+// TODO : Learn Error Handling
 const throwwErr = (req, res, next) => {
-  res.status(404);
-  throw new Error("The requested resource couldn't be found.");
+  let err = new Error("The requested resource couldn't be found.");
+  err.statusCode = 500;
+  err.stack = err.stack;
+  // res.on("finish", () => {});
+  next(err);
 };
-app.use(throwwErr);
+
+app.use(throwwErr, (err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode).send(err.message);
+});
 const port = 5000;
 app.listen(port, () => console.log("Server is listening on port", port));
