e938b42449ecaf224787a2d2966bc43045480b67
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-09 21:13:30 +0530

    Phase-1 ✅ use express-async-errors

--- Express/express-long-practice/server/app.js
+++ Express/express-long-practice/server/app.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
+// Use set NODE_ENV=production , to hide error log from non admins.
+// NOTE: express-async-errors prevents server to crash incase of errors, so must use.
+require("express-async-errors");
 const app = express();
 
+app.use("/static", express.static("assets"));
 // For testing purposes, GET /
-app.get('/', (req, res) => {
-  res.json("Express server running. No content provided at root level. Please use another route.");
+app.get("/", (req, res) => {
+  res.json(
+    "Express server running. No content provided at root level. Please use another route."
+  );
 });
 
+app.use(express.json());
 // For testing express.json middleware
-app.post('/test-json', (req, res, next) => {
+app.post("/test-json", (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -15,9 +22,9 @@ app.post('/test-json', (req, res, next) => {
 });
 
 // For testing express-async-errors
-app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+app.get("/test-error", async (req, res) => {
+  throw new Error("Hello World!");
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
