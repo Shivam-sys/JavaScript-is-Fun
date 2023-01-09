6165e5bb1eca0616ad248a68266dfe13b4c6dce3
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-09 21:49:54 +0530

    Phase -2, use finish listener

--- Express/express-long-practice/server/app.js
+++ Express/express-long-practice/server/app.js
@@ -4,9 +4,18 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  // use this listener to handle res object once res is done manipulated.
+  res.on("finish", () => {
+    // read and log the status code of the response
+    console.log(res.statusCode);
+  });
+  next();
+};
 app.use("/static", express.static("assets"));
 // For testing purposes, GET /
-app.get("/", (req, res) => {
+app.get("/", logger, (req, res) => {
   res.json(
     "Express server running. No content provided at root level. Please use another route."
   );
@@ -14,7 +23,7 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 // For testing express.json middleware
-app.post("/test-json", (req, res, next) => {
+app.post("/test-json", logger, (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -22,9 +31,14 @@ app.post("/test-json", (req, res, next) => {
 });
 
 // For testing express-async-errors
-app.get("/test-error", async (req, res) => {
+app.get("/test-error", logger, async (req, res) => {
   throw new Error("Hello World!");
 });
 
+const throwwErr = (req, res, next) => {
+  res.status(404);
+  throw new Error("The requested resource couldn't be found.");
+};
+app.use(throwwErr);
 const port = 5000;
 app.listen(port, () => console.log("Server is listening on port", port));
