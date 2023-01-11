12e459b9a5624539fe8ed43dbfeba410e7d4001b
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-11 16:01:29 +0530

    on delete cascade

--- Database/express-sql/server/seed-data.sql
+++ Database/express-sql/server/seed-data.sql
@@ -10,3 +10,10 @@ VALUES
   ('Red'),
   ('White'),
   ('Blue');
+
+-- To delete foreign keys from all child tables.
+-- Consider T2 as a child table and T1 as parent table.
+-- ALTER TABLE dbo.T2
+  --  ADD CONSTRAINT FK_T1_T2_Cascade
+  --  FOREIGN KEY (EmployeeID) REFERENCES dbo.T1(EmployeeID) ON DELETE CASCADE
+  -- https://my.appacademy.io/lessons/delete-cascade/e269baa1/practices/delete-dependent-data-with-delete-cascade/5d0a85fe
