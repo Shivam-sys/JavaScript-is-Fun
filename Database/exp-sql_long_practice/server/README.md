839362e58eb9ff3bb03a307d3d2f1ce21abc47c7
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-12 17:55:14 +0530

    Trees Long practice done

--- Database/exp-sql_long_practice/server/README.md
+++ Database/exp-sql_long_practice/server/README.md
@@ -1,3 +1,6 @@
 Run `sqlite3 app.db` to create the server/app.db database and open the SQLite CLI.
 Now, run `.read seed-data.sql` to run the .sql file having commands to create table and columns.
 Exit the SQLite CLI using `Ctrl+d`.
+
+then create .env file with DATA_SOURCE=dbname.db
+and use this in server file (trees.js or app.js)
--- Database/exp-sql_long_practice/server/routes/trees.js
+++ Database/exp-sql_long_practice/server/routes/trees.js
@@ -1,5 +1,5 @@
 // Instantiate router - DO NOT MODIFY
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /**
@@ -7,7 +7,9 @@ const router = express.Router();
  *   - Database file: "data_source" environment variable
  *   - Database permissions: read/write records in tables
  */
-// Your code here
+const sqlite3 = require("sqlite3");
+const DATA_SOURCE = process.env.DATA_SOURCE;
+const db = new sqlite3.Database(DATA_SOURCE, sqlite3.OPEN_READWRITE);
 
 /**
  * BASIC PHASE 2, Step B - List of all trees in the database
@@ -19,7 +21,15 @@ const router = express.Router();
  *   - Object properties: height-ft, tree, id
  *   - Ordered by the height_ft from tallest to shortest
  */
-// Your code here
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM trees ORDER BY tree;";
+  const params = [];
+  db.all(query, params, (err, rows) => {
+    if (err) {
+      next(err);
+    } else res.json(rows);
+  });
+});
 
 /**
  * BASIC PHASE 3 - Retrieve one tree with the matching id
@@ -30,7 +40,16 @@ const router = express.Router();
  * Response: JSON Object
  *   - Properties: id, tree, location, height_ft, ground_circumference_ft
  */
-// Your code here
+
+router.get("/:id", (req, res) => {
+  const query = "SELECT * FROM trees WHERE id=?";
+  const params = [req.params.id];
+  db.get(query, params, (err, row) => {
+    if (err) {
+      next(err);
+    } else res.json(row);
+  });
+});
 
 /**
  * INTERMEDIATE PHASE 4 - INSERT tree row into the database
@@ -42,7 +61,21 @@ const router = express.Router();
  *   - Property: message
  *   - Value: success
  */
-// Your code here
+router.post("/", (req, res) => {
+  const query =
+    "INSERT INTO trees(tree, location, height, ground_circumference_ft) VALUES(?,?,?,?);";
+  const params = [
+    req.body.name,
+    req.body.location,
+    req.body.height,
+    req.body.size,
+  ];
+
+  db.run(query, params, (err) => {
+    if (err) next(err);
+    else res.json({ message: "Successfully inserted" });
+  });
+});
 
 /**
  * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
@@ -54,7 +87,15 @@ const router = express.Router();
  *   - Property: message
  *   - Value: success
  */
-// Your code here
+
+router.delete("/:id", (req, res) => {
+  const query = "DELETE FROM trees WHERE id = ?";
+  const params = [req.params.id];
+  db.run(query, params, (err) => {
+    if (err) next(err);
+    else res.json({ message: "Successfully deleted" });
+  });
+});
 
 /**
  * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
@@ -66,7 +107,25 @@ const router = express.Router();
  *   - Property: message
  *   - Value: success
  */
-// Your code here
+router.put("/:id", (req, res) => {
+  if (req.params.id != req.body.id) {
+    res.status(400).json({ error: "ids do not match" });
+  } else {
+    const query =
+      "UPDATE trees SET tree=?, location=?, height=?, ground_circumference_ft=? WHERE id=?";
+    const params = [
+      req.body.name,
+      req.body.location,
+      req.body.height,
+      req.body.size,
+      req.body.id,
+    ];
+    db.run(query, params, (err) => {
+      if (err) next(err);
+      else res.json({ message: "success" });
+    });
+  }
+});
 
 // Export class - DO NOT MODIFY
 module.exports = router;
