1319668faaec6bf388388bb6a8a5c7151adb12c8
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-12 16:43:07 +0530

    phase 1 Seed data sql

--- Database/exp-sql_long_practice/server/seed-data.sql
+++ Database/exp-sql_long_practice/server/seed-data.sql
@@ -1,5 +1,36 @@
--- BASIC PHASE 1A - DROP and CREATE table
--- Your code here
-
--- BASIC PHASE 1B - INSERT seed data
--- Your code here
+-- DROP and CREATE table
+DROP TABLE IF EXISTS trees;
+CREATE TABLE trees (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tree VARCHAR(32),
+  location VARCHAR(64),
+  height NUMBER,
+  ground_circumference_ft NUMBER
+);
+-- Insert values
+INSERT INTO trees (tree, location, height, ground_circumference_ft)
+VALUES(
+    'General Sherman',
+    'Sequoia National Park',
+    274.9,
+    102.6
+  ),
+  (
+    'General Grant',
+    'Kings Canyon National Park',
+    268.1,
+    107.5
+  ),
+  (
+    'President',
+    'Sequoia National Park',
+    240.9,
+    93.0
+  ),
+  (
+    'Lincoln',
+    'Sequoia National Park',
+    255.8,
+    98.3
+  ),
+  ('Stagg', 'Private Land', 243.0, 109.0);
