ebbc806b63b801966c74fa5166652e54c4596c07
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-13 10:36:02 +0530

    Phase 1 completed

--- Database/advance-sql-prac/phase-1.sql
+++ Database/advance-sql-prac/phase-1.sql
@@ -1,8 +1,41 @@
 --Insert new cat named "Red" born this year
--- Your code here
-
+INSERT INTO cats (name, birth_year)
+VALUES ('Red', 2023);
 --Assign ownership of new cat to George Beatty using subqueries
--- Your code here
-
---Query to verify INSERTs worked properly
--- Your code here
+INSERT INTO cat_owners
+VALUES (
+    (
+      SELECT id
+      from owners
+      WHERE first_name = 'George'
+        AND last_name = 'Beatty'
+    ),
+    (
+      SELECT id
+      from cats
+      WHERE name = 'Red'
+    )
+  );
+-- Query to verify INSERTs worked properly
+SELECT *
+FROM cats;
+SELECT *
+FROM cat_owners;
+SELECT owners.first_name,
+  cats.name
+FROM owners,
+  cats
+WHERE owners.id =(
+    SELECT owner_id
+    FROM cat_owners
+    WHERE cat_id = (
+        SELECT id
+        FROM cats
+        WHERE name = 'Red'
+      )
+  )
+  AND cats.id = (
+    SELECT id
+    FROM cats
+    WHERE name = 'Red'
+  );
