c61d9ccbfb676b0154343ceeb4c37bb0053b8639
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-14 15:46:09 +0530

    Partial sequqlize

--- sequelize/exp-sequelize-1/config/database.js
+++ sequelize/exp-sequelize-1/config/database.js
@@ -1,23 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    storage: process.env.DB_FILE, //where to look for the database
+    dialect: "sqlite", //what type of database it is connecting to.
+    seederStorage: "sequelize", //track which seed files have been run in the database,
+    benchmark: true, //log the time it takes for Sequelize to execute each query
+    logQueryParameters: true, //log the values used as parameters in the SQL queries
+    typeValidation: true, //ey will prevent values from being inserted not having same type in database
+    // logging: false //by default uses console.log but can be overridden 
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+};
