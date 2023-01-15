43ab9deb46a8d05288bc836ff87c2e8352a59803
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-15 23:43:17 +0530

    Insertion in sequelize done

--- sequelize/insert-practice/server/app.js
+++ sequelize/insert-practice/server/app.js
@@ -1,40 +1,59 @@
 // Instantiate Express and the application - DO NOT MODIFY
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Error handling, env variables, and json middleware - DO NOT MODIFY
-require('express-async-errors');
-require('dotenv').config();
+require("express-async-errors");
+require("dotenv").config();
 app.use(express.json());
 
 // Import the models used in these routes - DO NOT MODIFY
-const { Puppy } = require('./db/models');
+const { Puppy } = require("./db/models");
 
 // Index of all puppies - DO NOT MODIFY
-app.get('/puppies', async (req, res, next) => {
-    const allPuppies = await Puppy.findAll({order: [['name', 'ASC']]});
+app.get("/puppies", async (req, res, next) => {
+  const allPuppies = await Puppy.findAll({ order: [["name", "ASC"]] });
 
-    res.json(allPuppies);
+  res.json(allPuppies);
 });
-
 
 // STEP 3
 // Capture the name, age_yrs, breed, weight_lbs, and microchipped attributes
 // from the body of the request.
 // Use these values to create a new Puppy in the database.
 // Respond to the request by sending a success message
-app.post('/puppies', async (req, res, next) => {
-    // Your code here
-})
-
+app.post("/puppies", async (req, res, next) => {
+  const {
+    name: puppyName,
+    age_yrs,
+    breed,
+    weight_lbs,
+    microchipped,
+  } = req.body;
+  try {
+    await Puppy.create({
+      name: puppyName,
+      age_yrs: age_yrs,
+      breed: breed,
+      weight_lbs: weight_lbs,
+      microchipped: microchipped,
+    });
+    res.status(201).json({
+      message: "Success",
+      data: await Puppy.findOne({ where: { name: puppyName } }),
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Root route - DO NOT MODIFY
-app.get('/', (req, res) => {
-    res.json({
-        message: "API server is running"
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "API server is running",
+  });
 });
 
 // Set port and listen for incoming requests - DO NOT MODIFY
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
