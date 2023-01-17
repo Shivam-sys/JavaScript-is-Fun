aa596b9ea2f853c3d3e16490ee7b8790da654214
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-17 20:50:30 +0530

    useState hook

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,5 +1,11 @@
+import UseState from "./components/UseState";
 function App() {
-  return <h1>react app</h1>;
+  return (
+    <>
+      <h1>react app</h1>
+      <UseState />
+    </>
+  );
 }
 
 export default App;
