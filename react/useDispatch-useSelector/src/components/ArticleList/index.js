aab6dbde559103c1dc4071e6788f59d5758f97d5
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-21 20:45:38 +0530

    used useSelector and useDispatcher

--- react/useDispatch-useSelector/src/components/ArticleList/index.js
+++ react/useDispatch-useSelector/src/components/ArticleList/index.js
@@ -1,9 +1,24 @@
-import { Route, Switch } from 'react-router-dom';
-import SingleArticle from '../SingleArticle';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SingleArticle from "../SingleArticle";
+import { loadArticles } from "../../store/articleReducer";
+import { NavLink } from "react-router-dom";
 
 const ArticleList = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.articleState.entries);
+
+  useEffect(() => {
+    dispatch(loadArticles());
+  }, [dispatch]);
   return (
     <div>
+      {articles.map(({ id, title }) => (
+        <li key={id}>
+          <NavLink to={`/article/${id}`}>{title}</NavLink>
+        </li>
+      ))}
       <h1>Article List</h1>
       <ol>
         <li>Gilligan's Island. Is it true?</li>
@@ -14,7 +29,7 @@ const ArticleList = () => {
       </ol>
 
       <Switch>
-        <Route path='/article/:id'>
+        <Route path="/article/:id">
           <SingleArticle />
         </Route>
       </Switch>
