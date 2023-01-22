facaeb64ed209ba094249591a2cc0b4940f016e7
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-22 23:45:36 +0530

    restoreUser and requireAuth setup

--- app-Authenticate-me/backend/utils/auth.js
+++ app-Authenticate-me/backend/utils/auth.js
@@ -26,3 +26,53 @@ const setTokenCookie = (res, user) => {
 
   return token;
 };
+
+/**
+ *
+ * If there is a User found, then save the user to a key of user onto the request.
+ * If there is an error verifying the JWT or a User cannot be found with the id,
+ * then clear the token cookie from the response.
+ */
+const restoreUser = (req, res, next) => {
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope("currentUser").findByPk(id);
+    } catch (e) {
+      res.clearCookie("token");
+      return next();
+    }
+
+    if (!req.user) res.clearCookie("token");
+
+    return next();
+  });
+};
+
+/**
+ * will ensure that if a valid JWT cookie exists, the session user will be loaded into the req.user attribute.
+ * The second middleware will check req.user and will go to the next middleware if there is a session user present there.
+ * If there is no session user, then an error will be created and passed along to the error-handling middlewares.
+ */
+
+const requireAuth = [
+  restoreUser,
+  function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error("Unauthorized");
+    err.title = "Unauthorized";
+    err.errors = ["Unauthorized"];
+    err.status = 401;
+    return next(err);
+  },
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
