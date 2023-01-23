6dc70079339707a86fe5e57c2bf0fce785691fd4
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-23 23:38:11 +0530

    Initial Setup Done!

--- app-Authenticate-me/frontend/src/index.js
+++ app-Authenticate-me/frontend/src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
 
-ReactDOM.render(
+const store = configureStore();
+
+// only for development environment
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Root />
+  </React.StrictMode>
 );
---
+++ app-Authenticate-me/frontend/src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({});
+
+// enhancer variable that will be set to different store enhancers
+// depending on if the Node environment is in development or production
+let enhancer;
+
+// In production, the enhancer should only apply the thunk middleware
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+/**
+ * configureStore function that takes in an optional preloadedState.
+ * Return createStore invoked with the rootReducer, the preloadedState, and the enhancer.
+ * @param {*} preloadedState
+ * @returns createStore
+ */
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
