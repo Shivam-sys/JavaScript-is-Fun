0e1e283407976fc9fe8de58cdcfd411f83c7cdcb
Shivam <tinkal.kumar.3766@gmail.com>
2023-01-24 23:58:05 +0530

    Login functionality Done ✅

--- app-Authenticate-me/frontend/src/App.js
+++ app-Authenticate-me/frontend/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
   return (
-    <Routes>
-      <Route path="/login" element={<LoginFormPage />} />
-    </Routes>
+    isLoaded && (
+      <Routes>
+        <Route path="/login" element={<LoginFormPage />} />
+      </Routes>
+    )
   );
 }
 
